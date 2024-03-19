4d54604c0ddaa22af8e52d368eecffb579e4707e
ali7haider <haider5568761@gmail.com>
2024-03-19 15:48:09 +0500

    Taking input with validation

--- main.py
+++ main.py
@@ -86,7 +86,7 @@
 #         self.workorder_input.clear()
 #         self.pt_input.clear()
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow,QMessageBox
 from PyQt5.QtCore import QPropertyAnimation, QEasingCurve, QUrl, Qt
 from PyQt5.QtGui import QIcon, QDesktopServices, QMouseEvent
 from main_ui import Ui_MainWindow  # Import the generated class
@@ -100,6 +100,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         # Set flags to remove the default title bar
         self.setWindowFlags(Qt.FramelessWindowHint)
+        # Set default page
+        self.stackedWidget.setCurrentIndex(1)
         
         # Connect the maximizeRestoreAppBtn button to the maximize_window method
         self.maximizeRestoreAppBtn.clicked.connect(self.maximize_window)
@@ -109,6 +111,33 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         # Connect the minimizeAppBtn button to the showMinimized method
         self.minimizeAppBtn.clicked.connect(self.showMinimized)
+        
+        # buttons to switch pages
+        self.btnHome.clicked.connect(lambda: self.change_page(0))
+        self.btnTimeTracking.clicked.connect(lambda: self.change_page(1))
+       
+        # buttons to Save record
+        self.btnSaveRecord.clicked.connect(self.save_record)
+
+    def save_record(self):
+            # Get inputs from line edits
+            txtID = self.txtID.text()
+            txtWO = self.txtWO.text()
+            txtPT = self.txtPT.text()
+            txtIssue = self.txtIssue.text()
+    
+            # Check if any input is blank
+            if txtID == '' or txtWO == '' or txtPT == '':
+                QMessageBox.warning(self, "Warning", "Please fill all required inputs.")
+            else:
+                # Print inputs for now
+                print("Input 1:", txtID)
+                print("Input 2:", txtWO)
+                print("Input 3:", txtPT)
+                print("Input 4:", txtIssue)
+    def change_page(self, index):
+        self.stackedWidget.setCurrentIndex(index)
+
     def mousePressEvent(self, event: QMouseEvent) -> None:
         if event.button() == Qt.LeftButton:
             self.dragPos = event.globalPos() - self.pos()
